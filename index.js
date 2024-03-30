17d9d35205b40829ea52eb460f64c5625955fa7c
djBrandy <dandobrandon0@gmail.com>
2024-03-30 17:54:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    if (winningRecord) {
+        return winningRecord.year;
+    } else {
+        return undefined; 
+    }
+}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+const winningYear = superbowlWin(record);
+console.log(winningYear); 
